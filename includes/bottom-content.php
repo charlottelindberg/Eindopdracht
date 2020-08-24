8a54da0e8c86b6d9892776f19bb99812c473f468
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-08-24 16:21:28 +0200

    Create bottom-content.php

---
+++ includes/bottom-content.php
@@ -0,0 +1,78 @@
+<hr>
+
+<!-- begin about us  -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-12">
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <h3>About Us</h3>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, consequatur neque
+                        exercitationem
+                        distinctio esse! Cupiditate doloribus a consequatur iusto illum eos facere vel iste iure
+                        maxime. Velit,
+                        rem, sunt obcaecati eveniet id nemo molestiae. In.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, consequatur neque
+                        exercitationem
+                        distinctio esse! Cupiditate doloribus a consequatur iusto illum eos facere vel iste iure
+                        maxime. Velit,
+                        rem, sunt obcaecati eveniet id nemo molestiae. In.</p>
+                </div>
+
+                <!-- end about us  -->
+
+
+                <!-- begin latest news  -->
+
+                <div class="col-lg-6 col-12">
+                    <h3>Latest News</h3>
+                    <hr>
+                    <div>
+                        <div class="media">
+                            <div class="media-body">
+                                <h4 class="mt-0 mb-1">Heading 1</h4>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quod temporibus
+                                veniam deserunt
+                                deleniti accusamus voluptatibus at illo sunt quisquam.
+                            </div>
+                            <a href="#"><img class="ml-3" src="images/75X.gif" alt="placeholder image"></a>
+                        </div>
+                        <div class="media">
+                            <div class="media-body">
+                                <h4 class="mt-0 mb-1">Heading 2</h4>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, iure nemo earum quae
+                                aliquid animi
+                                eligendi rerum rem porro facilis.
+                            </div>
+                            <a href="#"><img class="ml-3" src="images/75X.gif" alt="placeholder image"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- end latest news  -->
+
+
+        <!-- begin contact us  -->
+
+        <div class="col-md-4 col-12 mt-md-0 mt-2">
+            <h3>Contact Us</h3>
+            <hr>
+            <address>
+                <strong>MyStoreFront, Inc.</strong><br>
+                Indian Treasure Link<br>
+                Quitman, WA, 99110-0219<br>
+                <abbr title="Phone">P:</abbr> (123) 456-7890
+            </address>
+            <address>
+                <strong>Full Name</strong><br>
+                <a href="mailto:#">first.last@example.com</a>
+            </address>
+        </div>
+    </div>
+</div>
+
+<!-- end contact us  -->
