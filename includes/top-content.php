02965e4ba7e05c353d2d0fe441c121eeb6943f22
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-08-24 14:54:10 +0200

    Create top-content.php

---
+++ includes/top-content.php
@@ -0,0 +1,230 @@
+<!-- start hero carousel -->
+
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-12">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="http://placekitten.com/1920/500" alt="First slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Snowball</h5>
+                            <p>Twee jaar oud</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="http://placekitten.com/1920/500" alt="Second slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>KitKat</h5>
+                            <p>Vier jaar oud</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="http://placekitten.com/1920/500" alt="Third slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Pirrewiet</h5>
+                            <p>Zes manden oud</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <hr>
+</div>
+
+<!-- end hero carousel  -->
+
+
+<!-- begin 3 circle headings  -->
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-12">
+                <ul class="list-unstyled">
+                    <li class="media">
+                        <img class="mr-3 rounded-circle" src="images/100X100.gif" alt="Generic placeholder image">
+                        <div class="media-body">
+                            <h5 class="mt-0 mb-1">Heading 1</h5>
+                            <p class="mb-0">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante
+                                sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12">
+                <ul class="list-unstyled">
+                    <li class="media">
+                        <img class="mr-3 rounded-circle" src="images/100X100.gif" alt="Generic placeholder image">
+                        <div class="media-body ">
+                            <h5 class="mt-0 mb-1">Heading 2</h5>
+                            <p class="mb-0">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante
+                                sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4 d-md-none d-lg-block">
+                <ul class="list-unstyled">
+                    <li class="media">
+                        <img class="mr-3 rounded-circle" src="images/100X100.gif" alt="Generic placeholder image">
+                        <div class="media-body">
+                            <h5 class="mt-0 mb-1">Heading 3</h5>
+                            <p class="mb-0">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante
+                                sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- end 3 circle headings  -->
+
+
+<!-- begin 2x4 cards  -->
+
+<hr>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-12">
+            <div class="row">
+                <div class="col-sm-6 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-12">
+                    <div class="card">
+                        <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- end 2x4 cards  -->
+
+        <hr>
+
+        <!-- begin Selector Finder Tool  -->
+        <div class="col-lg-4 col-12">
+            <div class="row mx-0">
+                <div class="col-12 card bg-light mx-auto">
+                    <h3 class="text-center card-header">Vind <strong>jouw</strong> dier!</h3>
+                    <form class="card-body">
+                        <div class="form-group row">
+                            <label for="location1" class="col-form-label">Kies jouw regio</label>
+                            <select class="form-control" name="location" id="location1">
+                                <option value="">Alle provincies</option>
+                                <option value="">West-Vlaanderen</option>
+                                <option value="">Oost-Vlaanderen</option>
+                                <option value="">Vlaams Brabant</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="location2" class="col-form-label">Kies jouw diersoort</label>
+                            <select class="form-control" name="location" id="location2">
+                                <option value="">Alle diersoorten</option>
+                                <option value="">Honden</option>
+                                <option value="">Katten</option>
+                                <option value="">Konijnen</option>
+                                <option value="">Paarden</option>
+                                <option value="">Vogels</option>
+                                <option value="">Knaagdieren</option>
+                                <option value="">Reptielen</option>
+                                <option value="">Andere diersoorten</option>
+                            </select>
+                        </div>
+                        <p class="text-center"><a href="#" class="btn btn-danger" role="button">Search </a></p>
+                    </form>
+                </div>
+
+                <!-- end Selector Finder Tool  -->
+
+
+                <!-- begin user agent  -->
+                <div class="col-12">
+                    <h3 class="text-center">Agents</h3>
+                    <hr>
+                    <div class="media">
+                        <img class="mr-3" src="images/64X64.gif" alt="Generic placeholder image">
+                        <div class="media-body">
+                            <h5 class="mt-0">John Doe</h5>
+                            <abbr title="Phone">P:</abbr> (123) 456-7890 <a href="mailto:#">first.last@example.com</a>
+                        </div>
+                    </div>
+                    <div class="media mt-2">
+                        <img class="mr-3" src="images/64X64.gif" alt="Generic placeholder image">
+                        <div class="media-body">
+                            <h5 class="mt-0">Linda Smith</h5>
+                            <abbr title="Phone">P:</abbr> (123) 456-7890 <a href="mailto:#">first.last@example.com</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- end user agent  -->
