261c13cd200b6fae57dadd6e6bef30e1cf620e8f
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-08-26 16:03:20 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,7 @@
 
     <!-- begin Navbar  -->
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
         <a class="navbar-brand" href="#">Vind een dier!</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
             aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,9 +26,6 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
