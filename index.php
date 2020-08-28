c0b0dda5808430a289f24e3aa47523770d725952
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-08-28 15:57:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -183,14 +183,14 @@ include("includes/header.php");
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
                         <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
                             role="tab" aria-controls="nav-home" aria-selected="true">Honden</a>
-                        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
-                            role="tab" aria-controls="nav-profile" aria-selected="false">Katten</a>
-                        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact"
-                            role="tab" aria-controls="nav-contact" aria-selected="false">Konijnen</a>
-                        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact"
-                            role="tab" aria-controls="nav-contact" aria-selected="false">Kippen</a>
-                        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact"
-                            role="tab" aria-controls="nav-contact" aria-selected="false">Vogels</a>
+                        <a class="nav-item nav-link" id="nav-katten-tab" data-toggle="tab" href="#nav-katten"
+                            role="tab" aria-controls="nav-katten" aria-selected="false">Katten</a>
+                        <a class="nav-item nav-link" id="nav-konijnen-tab" data-toggle="tab" href="#nav-konijnen"
+                            role="tab" aria-controls="nav-konijnen" aria-selected="false">Konijnen</a>
+                        <a class="nav-item nav-link" id="nav-kippen-tab" data-toggle="tab" href="#nav-kippen"
+                            role="tab" aria-controls="nav-kippen" aria-selected="false">Kippen</a>
+                        <a class="nav-item nav-link" id="nav-paarden-tab" data-toggle="tab" href="#nav-paarden"
+                            role="tab" aria-controls="nav-paarden" aria-selected="false">Paarden</a>
                     </div>
                 </nav>
                 <div class="tab-content" id="nav-tabContent">
@@ -210,7 +210,7 @@ include("includes/header.php");
                         </p>
                         
                     </div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                    <div class="tab-pane fade" id="nav-katten" role="tabpanel" aria-labelledby="nav-katten-tab">
                         <p>
                         Voorkom probleemgedrag en schade aan de meubels door je huis katvriendelijk in te richten. Van alle benodigdheden heb je er één meer nodig dan het aantal katten in huis. Voor één kat zijn er dus twee kattenbakken nodig, voor twee katten drie kattenbakken enzovoort.
                         <p>
@@ -219,11 +219,11 @@ include("includes/header.php");
                         Het voer en het drinken mogen niet in de buurt van de kattenbak staan. Dit kan er toe   leiden dat de kat zijn behoefte op ongewenste plaatsen in huis doet. Zorg ook dat het voer en het drinken niet naast mekaar staan: laat er minstens 1,5 meter tussen. Katten houden er niet van om te drinken in de buurt van hun eten. Daardoor drinken ze soms te weinig, wat de kans op blaasproblemen verhoogt.
                         </p>
                     </div>
-                    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                    <div class="tab-pane fade" id="nav-konijnen" role="tabpanel" aria-labelledby="nav-konijnen-tab">
                         <p>
-                        In tegenstelling tot wat vaak wordt gedacht, zijn konijnen <strong> geen goede huisdieren voor kinderen</strong>. De meeste konijnen vinden oppakken en knuffelen niet leuk. Ze kunnen dan krabben en bijten en zelfs hun eigen rug breken als ze spartelen bij het optillen. 
+                        In tegenstelling tot wat vaak wordt gedacht, zijn konijnen <strong> geen goede huisdieren voor kinderen</strong>. De meeste konijnen vinden oppakken en knuffelen niet leuk. Ze kunnen dan krabben en bijten en <em> zelfs hun eigen rug breken</em> als ze spartelen bij het optillen. 
                         <p>
-                        Door veel tijd aan het konijn te besteden en er rustig mee om te gaan, kan het wel tam worden en leren dat aaien prettig kan zijn. Konijnen kunnen ook allerlei trucjes leren en er wordt zelfs aan konijnen-agility gedaan. Konijnen zijn dus vooral leuke huisdieren voor mensen die er ofwel veel mee willen bezig zijn, ofwel er vooral willen naar kijken.</p>
+                        Door veel tijd aan het konijn te besteden en er rustig mee om te gaan, kan het wel tam worden en leren dat aaien prettig kan zijn. Konijnen kunnen ook allerlei trucjes leren en er wordt zelfs aan konijnen-agility gedaan. <em> Konijnen zijn dus vooral leuke huisdieren voor mensen die er ofwel veel mee willen bezig zijn, ofwel er vooral willen naar kijken.</em></p>
                         </p>
                         <p>
                         Konijnen hebben veel ruimte nodig. Combineer een hok of kooi met een grote (buiten)ren en richt deze in met veel verstopplekjes en verschillende niveaus.
@@ -232,15 +232,27 @@ include("includes/header.php");
                         Een konijn is een groepsdier. <strong> Hou er minstens twee</strong>. De combinatie van een vrouwelijk konijn en een gecastreerd mannelijk konijn gaat het best. Twee mannetjes of twee vrouwtjes samen geeft meestal ruzie. 
                         </p>
                     </div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                        <p>
-                        
-                        </p>
-                    </div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                    <div class="tab-pane fade" id="nav-kippen" role="tabpanel" aria-labelledby="nav-kippen-tab">
                         <p>
                         Kippen bestaan in veel maten, kleuren en temperamenten. Er zijn rassen die vooral geschikt zijn om veel eieren te leggen en andere worden vooral gehouden voor het vlees of voor de sierwaarde. Naast echte raskippen worden er ook veel kruisingen gekweekt, de hybriden. Deze hybriden geven een veel grotere opbrengst van eieren of vlees dan de zuivere rassen en worden daarom veel in de industrie gebruikt. Ook de meeste legkippen die je in de dierenwinkel kan kopen, zijn hybriden. Ze leven doorgaans echter minder lang en zijn minder sterk dan de zuivere raskippen.
                         </p>
+                        <p>
+                        Om de kippen te vervoeren, kan je een speciaal kippenkrat gebruiken of een stevige kartonnen doos met luchtgaten. Zo kunnen de kippen veilig en rustig vervoerd worden. Zet de doos goed vast in de auto, zodat deze niet aan het schuiven gaat. Kippen geraken snel oververhit. Transporteer ze dus niet op warme dagen en zorg voor voldoende ventilatie.
+                        </p>
+                        <p>
+                        Richt het hok en de ren in voordat je de kippen koopt. Zet de kippen bij aankomst in het binnenhok en laat hen zelf de weg naar buiten ontdekken. Geef in het begin hetzelfde voer als de vorige eigenaar en wen ze geleidelijk aan nieuw voer. 
+                        </p>
+                    </div>
+                    <div class="tab-pane fade" id="nav-paarden" role="tabpanel" aria-labelledby="nav-paarden-tab">
+                        <p>
+                        Paarden zijn kuddedieren die bij voorkeur in een kleine kudde leven. Andere dieren, zoals geiten of ezels, kunnen ook op dezelfde weide staan, maar ze vervangen het gezelschap van een ander paard niet.</p>
+                        <p>
+                        Als je zelf maar één paard kan houden, zorg er dan voor dat het dier andere paarden kan zien of probeer met andere paardeneigenaars af te spreken om de dieren samen te zetten.
+                        </p>
+                        <p>
+                        Als je een nieuw paard in de groep brengt, dan hou je het eerst een aantal weken apart in een aangrenzende weide. Zo leren de dieren elkaar eerst kennen zonder dat ze een bedreiging vormen voor elkaar. Als de dieren gezond zijn en ze elkaar goed kennen en verdragen, dan kan je ze voor het eerst samen zetten. Voorkom agressie over voeder door op meerdere plaatsen hooi en water aan te bieden.
+                        </p>
+                        
                     </div>
                 </div>
             </div>
