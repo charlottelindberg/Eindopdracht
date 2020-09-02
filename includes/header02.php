c82bb2d5cadb2f3c77f7bc1881fa5a7a746fc9cc
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-09-02 15:00:37 +0200

    Create header02.php

---
+++ includes/header02.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vind een dier!</title>
+
+    <!-- favicon -->
+    <!-- <link rel="icon" href="/favicon.ico">  -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap-mint.css" rel="stylesheet">
+
+    <!-- eigen CSS  -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- eigen JS  -->
+    <link rel="script" href="/js/javascript.js">
+</head>
+
+<body>
+
+    <!-- begin Navbar  -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+        <a class="navbar-brand" href="#">
+        <img src="./images/logo.png" width="155" height="50" alt="logo" loading="lazy">
+        </a>
+               <ul class="navbar-nav mr-auto">
+                <li class="navbar-text text-black-50 text-center">
+                <!-- Vind <strong>jouw</strong> dier! -->
+                </li>
+            </ul>
+       
+            <span class="navbar-text">
+                <a class="nav-link text-black-50" href="login.php">Aanmelden</a>
+            </span>
+        </div>
+    </nav>
+    <!-- end Navbar  -->
