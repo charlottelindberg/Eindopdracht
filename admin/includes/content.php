8a624f824c11ce03f7225fadd100c59f4af8606b
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-09-04 15:06:32 +0200

    Update content.php

--- admin/includes/content.php
+++ admin/includes/content.php
@@ -1,15 +1,13 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-    <!-- Page Heading -->
+    
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+        <h1 class="h3 mb-0 text-gray-800">Vind een dier - Dashboard</h1>
     </div>
-    <!-- Content Row -->
-    <div class="row">
+    
 
-        <!-- Earnings (Monthly) Card Example -->
+    <div class="row">   
         <div class="col-12 col-md-4 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
@@ -26,7 +24,7 @@
             </div>
         </div>
 
-        <!-- Earnings (Monthly) Card Example -->
+        
         <div class="col-12 col-md-4 mb-4">
             <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
@@ -43,7 +41,7 @@
             </div>
         </div>
 
-        <!-- Earnings (Monthly) Card Example -->
+        
         <div class="col-12 col-md-4 mb-4">
             <div class="card border-left-info shadow h-100 py-2">
                 <div class="card-body">
@@ -65,7 +63,6 @@
         </div>
     </div>
 
-    <!-- Content Row -->
 
 <div class="row">
     <div id="piechart" style="width: 900px; height: 500px;"></div>
