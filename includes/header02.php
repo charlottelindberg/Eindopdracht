4a0dd056f082ce89ebf33ded76e52c3321eb511a
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-09-04 09:57:44 +0200

    Update header02.php

--- includes/header02.php
+++ includes/header02.php
@@ -34,12 +34,35 @@
         <a class="navbar-brand" href="#">
         <img src="./images/logo.png" width="155" height="50" alt="logo" loading="lazy">
         </a>
-               <ul class="navbar-nav mr-auto">
-                <li class="navbar-text text-black-50 text-center">
-                <!-- Vind <strong>jouw</strong> dier! -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-black-50" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dierenasielen
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Asiel #1</a>
+                        <a class="dropdown-item" href="#">Asiel #2</a>
+                        <a class="dropdown-item" href="#">Asiel #3</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-black-50" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dierensoorten
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Link naar type dier + alle regios</a>
+                        <a class="dropdown-item" href="#">Link naar type dier + alle regios</a>
+                        <a class="dropdown-item" href="#">Link naar type dier + alle regios</a>
+                    </div>
                 </li>
             </ul>
-       
             <span class="navbar-text">
                 <a class="nav-link text-black-50" href="login.php">Aanmelden</a>
             </span>
