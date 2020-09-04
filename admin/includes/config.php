688a1a9ae14c39fdffa453f95d086289cadde69c
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-09-04 15:06:28 +0200

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost:3308');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'dbblogoop');
+define('DB_NAME', 'vindeendierdb');
 ?>
