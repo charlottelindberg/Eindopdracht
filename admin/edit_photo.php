c6f04394e092f62aff22be151e789ac6574c7faa
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-09-16 16:08:23 +0200

    Update edit_photo.php

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -10,7 +10,6 @@ if(empty($_GET['id'])){
     $photo = Photo::find_by_id($_GET['id'], 'FotoID');
     if(isset($_POST['update'])){
         if($photo){
-            $photo->FotoID = $_POST['FotoID'];
             $photo->Bestandsnaam = $_POST['Bestandsnaam'];
             $photo->Dier_ID = $_POST['Dier_ID'];
             $photo->update();
@@ -30,59 +29,26 @@ if(empty($_GET['id'])){
             <form action="edit_photo.php?id=<?php echo $photo->id; ?>" method="post">
                 <div class="col-md-8">
                     <div class="form-group">
-                        <label for="FotoID">FotoID</label>
-                        <input type="text" name="FotoID" class="form-control" value="<?php echo $photo->FotoID; ?>">
+                        <a href="#" class="thumbnail"><img src="<?php echo $photo->picture_path(); ?>" height="400" width="auto" alt=""></a>
                     </div>
                     <div class="form-group">
-                        <a href="#" class="thumbnail"><img src="<?php echo $photo->picture_path(); ?>" alt=""></a>
-                    </div>
-                    <div class="form-group">
-                        <label for="Bestandsnaam">Caption</label>
+                        <label for="Bestandsnaam">Naam</label>
                         <input type="text" name="Bestandsnaam" class="form-control" value="<?php echo $photo->Bestandsnaam; ?>">
                     </div>
                     <div class="form-group">
-                        <label for="alternate_text">Alternate text</label>
-                        <input type="text" name="alternate_text" class="form-control" value="<?php echo $photo->alternate_text; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="Dier_ID">Description</label>
+                        <label for="Dier_ID">Omschrijving</label>
                         <textarea class="form-control" name="Dier_ID" id="" cols="30" rows="10"><?php echo $photo->Dier_ID; ?></textarea>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="photo-info-box">
-                        <div class="info-box-header">
-                            <h4>Save <span id="toggle" class="fas fa-arrow-up"></span></h4>
-                        </div>
-                        <div class="inside">
-                            <div class="box-inner">
-                                <p class="text">
-                                    <span class="fas fa-calendar">Uploaded on: April, 15, 2019 @ 7:54</span>
-                                </p>
-                                <p class="text">
-                                    <span class="data photo_id_box">Uploaded on: April, 15, 2019 @ 7:54</span>
-                                </p>
-                                <p class="text">
-                                    Photo id: <span class="data">34</span>
-                                </p>
-                                <p class="text">
-                                    Filename: <span class="data">image.jpg</span>
-                                </p>
-                                <p class="text">
-                                    <label for="type">File Type</label>
-                                    <input readonly type="text" name="type" class="form-control" value="<?php echo $photo->type; ?>">
-                                </p>
-                                <p class="text">
-                                    <label for="size">File Size</label>
-                                    <input readonly type="text" name="size" class="form-control" value="<?php echo $photo->size; ?>">
-                                </p>
-                            </div>
-                            <div class="info-box-footer float-left">
-                                <div class="info-box-delete pull-left">
-                                    <a href="delete_photo.php?id=<?php echo $photo->id; ?>" class="btn btn-danger btn-lg">Delete</a>
+                                           
+                            
+                            <div class="info-box-footer">
+                                <div class="info-box-delete">
+                                    <a href="delete_photo.php?id=<?php echo $photo->FotoID; ?>" class="btn btn-danger btn-lg">Delete</a>
                                 </div>
-                                <div class="info-box-update float-right">
-                                    <input type="submit" name="update" value="update" class="btn btn-primary btn-lg">
+                                <div class="info-box-update">
+                                <a href="edit_photo.php?id=<?php echo $photo->FotoID; ?>"
+                                   class="btn btn-primary btn-lg">Update</a>
                                 </div>
                             </div>
                         </div>
