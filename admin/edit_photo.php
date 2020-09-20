256a2e37dd0ca81605c0fa273306707cff15891a
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-09-20 17:47:38 +0200

    Update edit_photo.php
    kommentaar sectie updated

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -1,15 +1,15 @@
 <?php include("includes/header.php");
 
-if(!$session->is_signed_in()){
+if (!$session->is_signed_in()) {
     redirect('login.php');
 }
 $foto = Photo::find_all();
-if(empty($_GET['id'])){
+if (empty($_GET['id'])) {
     redirect('photos.php');
-}else{
+} else {
     $photo = Photo::find_by_id($_GET['id'], 'FotoID');
-    if(isset($_POST['update'])){
-        if($photo){
+    if (isset($_POST['update'])) {
+        if ($photo) {
             $photo->Bestandsnaam = $_POST['Bestandsnaam'];
             $photo->Dier_ID = $_POST['Dier_ID'];
             $photo->update();
@@ -20,16 +20,16 @@ if(empty($_GET['id'])){
 
 <?php include("includes/sidebar.php"); ?>
 
-<?php include("includes/content-top.php");?>
+<?php include("includes/content-top.php"); ?>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <h2>Welkom op de edit photo pagina</h2>
+            <h2>Welkom op de edit foto pagina</h2>
             <form action="edit_photo.php?id=<?php echo $photo->FotoID; ?>" method="post">
                 <div class="col-md-8">
                     <div class="form-group">
-                        <a href="#" class="thumbnail"><img src="<?php echo $photo->picture_path(); ?>" height="400" width="auto" alt=""></a>
+                        <img src="<?php echo $photo->picture_path(); ?>" alt=""></>
                     </div>
                     <div class="form-group">
                         <label for="Bestandsnaam">Naam</label>
@@ -37,28 +37,24 @@ if(empty($_GET['id'])){
                     </div>
                     <div class="form-group">
 
-                    <!-- dit moet naar dieren leiden in sql, en niet naar fotos  -->
+                        <!-- dit moet naar dieren leiden in sql, en niet naar fotos  -->
                         <label for="Dier_ID">Omschrijving</label>
                         <textarea class="form-control" name="Dier_ID" id="" cols="30" rows="10"><?php echo $photo->Dier_ID; ?></textarea>
                     </div>
                 </div>
-                                           
-                            
-                            <div class="info-box-footer">
-                                <div class="info-box-delete">
-                                    <a href="delete_photo.php?id=<?php echo $photo->FotoID; ?>" class="btn btn-danger btn-lg">Delete</a>
-                                </div>
-                                <div class="info-box-update">
-                                <a href="edit_photo.php?id=<?php echo $photo->FotoID; ?>"
-                                   class="btn btn-primary btn-lg">Update</a>
-                                </div>
-                            </div>
-                        </div>
+
+
+                <div class="info-box-footer">
+                    <div class="info-box-update">
+                        <a href="edit_photo.php?id=<?php echo $photo->FotoID; ?>" class="btn btn-success btn-lg">Update</a>
                     </div>
                 </div>
-            </form>
         </div>
     </div>
 </div>
+</form>
+</div>
+</div>
+</div>
 
-<?php include("includes/footer.php");?>
+<?php include("includes/footer.php"); ?>
