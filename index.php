92a70d93731b912be7ec75e25fd2dc123065ad04
Charlotte Lindberg <charlottelindberg@outlook.com>
2020-09-24 15:15:11 +0200

    Update index.php
    <hr>

--- index.php
+++ index.php
@@ -81,6 +81,7 @@ $photos = Photo::find_this_query($sql);
                 <h3 class="pt-3 text-center">
                     Zorgen voor huisdieren
                 </h3>
+                <hr>
                 <nav>
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
                         <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Honden</a>
